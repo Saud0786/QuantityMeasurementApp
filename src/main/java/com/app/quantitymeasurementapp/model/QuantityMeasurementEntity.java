3f57be188010500b092cded79a2d46845ff6f0b7
Saud0786 <saud31253@gmail.com>
2026-03-16 15:52:22 +0530

    [Saud] : intial setup of uc-17

---
+++ src/main/java/com/app/quantitymeasurementapp/model/QuantityMeasurementEntity.java
@@ -0,0 +1,5 @@
+package com.app.quantitymeasurementapp.model;
+
+public class QuantityMeasurementEntity {
+
+}
---
+++ src/main/java/com/app/quantitymeasurementapp/unit/SupportArithemetic.java
@@ -0,0 +1,6 @@
+package com.app.quantitymeasurementapp.unit;
+
+@FunctionalInterface
+interface SupportArithemetic {
+   boolean isSupported();
+}
