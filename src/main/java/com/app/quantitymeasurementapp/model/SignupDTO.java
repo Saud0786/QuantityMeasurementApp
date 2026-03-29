c06eae25dd801937a6e9671cff75adc2a98eb7df
Saud0786 <saud31253@gmail.com>
2026-03-29 15:46:34 +0530

    [Saud]: Added SignUp Data transfer object

---
+++ src/main/java/com/app/quantitymeasurementapp/model/SignupDTO.java
@@ -0,0 +1,14 @@
+package com.app.quantitymeasurementapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignupDTO {
+    private String name;
+    private String email;
+    private String password;
+}
